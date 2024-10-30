8fd877030b9db3186fa62109bf434c9ef1447584
Darwin Acharya <acharyadarwin3@gmail.com>
2024-10-30 16:23:30 +0545

    add image, description and link

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png",width=500)
+    st.image("images/photo.png")
     
 with col2:
     st.title("Darwin Acharya")
@@ -28,15 +28,21 @@ Below you can find some of the apps that I have built in python.
 Feel free to contact me!!"""    
 st.info(content2)
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv("data.csv",sep=";")
 
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code({row['url']})]")
         
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code({row['url']})]")
     
