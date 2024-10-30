3be6d0fe4fa21b5f66e341298c226bbd8882fad5
Darwin Acharya <acharyadarwin3@gmail.com>
2024-10-30 16:03:35 +0545

    adding name of the project

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png",width=500)
     
 with col2:
     st.title("Darwin Acharya")
     content = """
-    Hey, I am Darwin Acharya currently studying bachelors in electronics
-    communication and information engineering in Advanced College of 
-    Engineering and Management.
+    Hey, My name is Darwin Acharya, and I am currently pursuing a
+    bachelor's degree in Electronics Communication and Information
+    Engineering at the Advanced College of Engineering and Management
+    in Kathmandu, Nepal. I have a strong passion for learning,
+    especially in coding and technology, and I enjoy exploring
+    new ideas. Although I am still in the early stages of my career,
+    I aspire to build a future in engineering, focusing on machine
+    learning, deep learning, and artificial intelligence. I'm excited 
+    about the innovative opportunities these fields present as I continue
+    my studies and embrace new challenges.
     """
     st.info(content)
+    
+content2 = """
+Below you can find some of the apps that I have built in python. 
+Feel free to contact me!!"""    
+st.info(content2)
+
+col3,col4 = st.columns(2)
+
+df = pd.read_csv("data.csv",sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row['title'])
+        
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row['title'])
+    
